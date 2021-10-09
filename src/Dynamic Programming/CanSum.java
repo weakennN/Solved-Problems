3ae85974ff55ee5ddfa29b66b0d3e955b0274a60
weakennN <lubomedi03@gmail.com>
2021-10-09 22:58:04 +0300

    added some solved problems

---
+++ src/Dynamic Programming/CanSum.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class CanSum {
+    private static int counter = 0;
+
+    public static void main(String[] args) {
+        System.out.println(canSum(1000, new int[]{3,1}, new HashMap<>()));
+    }
+
+    private static boolean canSum(int totalSum, int[] nums, HashMap<Integer, Boolean> memo) {
+        if (memo.containsKey(totalSum)) {
+            return memo.get(totalSum);
+        } else if (totalSum == 0) {
+            return true;
+        } else if (totalSum < 0) {
+            return false;
+        }
+
+        System.out.println(++counter);
+
+        for (int i = 0; i < nums.length; i++) {
+            memo.put(totalSum, canSum(totalSum - nums[i], nums, memo));
+            if (memo.get(totalSum)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/Dynamic Programming/FrogRiverProblem.java
@@ -0,0 +1,23 @@
+public class FrogRiverProblem {
+    private static int countTimes = 0;
+
+    public static void main(String[] args) {
+        System.out.println(frogRiver(2, new int[]{1, 2}));
+    }
+
+    private static int frogRiver(int destination, int[] nums) {
+        if (destination == 0) {
+            return 1;
+        } else if (destination < 0) {
+            return 0;
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            total += frogRiver(destination - nums[i], nums);
+        }
+
+        return total;
+    }
+}
