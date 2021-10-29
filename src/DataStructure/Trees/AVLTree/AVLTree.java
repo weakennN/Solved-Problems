ef276b9288ff07d210a36a7bd4887350721004d7
weakennN <lubomedi03@gmail.com>
2021-10-29 23:06:14 +0300

    updated AVL tree

--- src/DataStructure/Trees/AVLTree/AVLTree.java
+++ src/DataStructure/Trees/AVLTree/AVLTree.java
@@ -12,6 +12,11 @@ public class AVLTree<E extends Comparable<E>> {
         this.root = this.recursiveInsert(this.root, element);
     }
 
+    public void remove(E element) {
+        this.root = this.recursiveRemove(this.root, element);
+        this.root = this.balance(this.root);
+    }
+
     private Node<E> recursiveInsert(Node<E> node, E element) {
         if (node == null) {
             return new Node<>(element);
@@ -37,6 +42,61 @@ public class AVLTree<E extends Comparable<E>> {
                 return this.leftRotation(node);
             } else {
                 node.setRight(this.rightRotation(node.getRight()));
+                return this.leftRotation(node);
+            }
+        }
+
+        return node;
+    }
+
+    private Node<E> recursiveRemove(Node<E> node, E element) {
+        if (node == null) {
+            return null;
+        } else if (node.getElement().compareTo(element) == 0) {
+            if (node.getRight() == null && node.getLeft() == null) {
+                return null;
+            } else {
+                Node<E> replace = this.getInorderSuccessor(node);
+                if (replace == null) {
+                    replace = this.getInorderPredecessor(node);
+                }
+
+                this.recursiveRemove(node, replace.getElement());
+                replace.setLeft(node.getLeft());
+                replace.setRight(node.getRight());
+
+                return replace;
+            }
+        }
+
+        if (node.getElement().compareTo(element) > 0) {
+            node.setLeft(this.recursiveRemove(node.getLeft(), element));
+        } else if (node.getElement().compareTo(element) < 0) {
+            node.setRight(this.recursiveRemove(node.getRight(), element));
+        }
+
+        int balanceFactor = this.calculateHeight(node.getLeft()) - this.calculateHeight(node.getRight());
+
+        if (balanceFactor >= 2) {
+            int leftChildBalanceFactor = this.calculateHeight(node.getLeft().getLeft())
+                    - this.calculateHeight(node.getLeft().getRight());
+            if (leftChildBalanceFactor >= 1) {
+                return this.rightRotation(node);
+            } else if (leftChildBalanceFactor <= -1) {
+                node.setLeft(this.leftRotation(node.getLeft()));
+                return this.rightRotation(node);
+            } else {
+                return this.rightRotation(node);
+            }
+        } else if (balanceFactor <= -2) {
+            int rightChildBalanceFactor = this.calculateHeight(node.getRight().getLeft())
+                    - this.calculateHeight(node.getRight().getRight());
+            if (rightChildBalanceFactor <= -1) {
+                return this.leftRotation(node);
+            } else if (rightChildBalanceFactor >= 1) {
+                node.setRight(this.rightRotation(node.getRight()));
+                return this.leftRotation(node);
+            } else {
                 return this.leftRotation(node);
             }
         }
@@ -74,5 +134,60 @@ public class AVLTree<E extends Comparable<E>> {
 
     public Node<E> getRoot() {
         return this.root;
+    }
+
+    private Node<E> getInorderSuccessor(Node<E> node) {
+        Node<E> current = node.getRight();
+        while (current != null && current.getLeft() != null) {
+            current = current.getLeft();
+        }
+
+        return current;
+    }
+
+    private Node<E> getInorderPredecessor(Node<E> node) {
+        Node<E> current = node.getLeft();
+        while (current != null && current.getRight() != null) {
+            current = current.getRight();
+        }
+
+        return current;
+    }
+
+    private Node<E> balance(Node<E> node) {
+        if (node == null) {
+            return null;
+        }
+
+        node.setLeft(balance(node.getLeft()));
+        node.setRight(balance(node.getRight()));
+
+        int balanceFactor = this.calculateHeight(node.getLeft()) - this.calculateHeight(node.getRight());
+
+        if (balanceFactor >= 2) {
+            int leftChildBalanceFactor = this.calculateHeight(node.getLeft().getLeft())
+                    - this.calculateHeight(node.getLeft().getRight());
+            if (leftChildBalanceFactor >= 1) {
+                return this.rightRotation(node);
+            } else if (leftChildBalanceFactor <= -1) {
+                node.setLeft(this.leftRotation(node.getLeft()));
+                return this.rightRotation(node);
+            } else {
+                return this.rightRotation(node);
+            }
+        } else if (balanceFactor <= -2) {
+            int rightChildBalanceFactor = this.calculateHeight(node.getRight().getLeft())
+                    - this.calculateHeight(node.getRight().getRight());
+            if (rightChildBalanceFactor <= -1) {
+                return this.leftRotation(node);
+            } else if (rightChildBalanceFactor >= 1) {
+                node.setRight(this.rightRotation(node.getRight()));
+                return this.leftRotation(node);
+            } else {
+                return this.leftRotation(node);
+            }
+        }
+
+        return node;
     }
 }
--- src/DataStructure/Trees/AVLTree/AVLTreeTest.java
+++ src/DataStructure/Trees/AVLTree/AVLTreeTest.java
@@ -3,12 +3,35 @@ package DataStructure.Trees.AVLTree;
 public class AVLTreeTest {
 
     public static void main(String[] args) {
-        AVLTree<Integer> avlTree = new AVLTree<>(811);
-        avlTree.insert(488);
-        avlTree.insert(679);
-        avlTree.insert(600);
+        AVLTree<Integer> avlTree = new AVLTree<>(14);
+        avlTree.insert(11);
+        avlTree.insert(19);
+        avlTree.insert(7);
+        avlTree.insert(12);
+        avlTree.insert(17);
+        avlTree.insert(53);
+        avlTree.insert(13);
+        avlTree.insert(4);
+        avlTree.insert(8);
+        avlTree.insert(16);
+        avlTree.insert(20);
+        avlTree.insert(60);
 
+        avlTree.remove(8);
         inorder(avlTree.getRoot());
+        System.out.println();
+        avlTree.remove(7);
+        inorder(avlTree.getRoot());
+        System.out.println();
+        avlTree.remove(11);
+        inorder(avlTree.getRoot());
+        System.out.println();
+        avlTree.remove(14);
+        inorder(avlTree.getRoot());
+        System.out.println();
+        avlTree.remove(17);
+        inorder(avlTree.getRoot());
+        System.out.println();
     }
 
     private static void inorder(Node<Integer> node) {
@@ -19,6 +42,16 @@ public class AVLTreeTest {
         inorder(node.getLeft());
         System.out.print(node.getElement() + " ");
         inorder(node.getRight());
+    }
+
+    private static void preorder(Node<Integer> node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.getElement());
+        preorder(node.getLeft());
+        preorder(node.getRight());
     }
 
 }
